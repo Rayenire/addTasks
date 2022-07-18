d3c8f6032383c772aaa328d4ce16efd8356b668a
Rayenire <aeristarg@gmail.com>
2022-07-18 17:51:29 +0300

    Add task 1

---
+++ task01/Program.cs
@@ -0,0 +1,6 @@
+﻿// Улитка ползёт по вертикальному шесту высотой h метров, поднимаясь за день на a метров, 
+//а за ночь спускаясь на b метров. На какой день улитка доползёт до вершины шеста?
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int h = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Улитка доползёт до вершины шеста за {h / (a - b)} дней");
